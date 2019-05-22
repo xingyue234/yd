a193f2a8cc4a2622a9fc7cb85f0407e6832b4321
朱田丽 <1780982620@qq.com>
2019-05-22 16:30:23 +0800

    ficrst

---
+++ yd10.0/js/config.js
@@ -0,0 +1,165 @@
+
+var config = {
+  'http://zz.henie.top': {
+    company_name: '贵州元和翰商贸有限公司',       // 公司名称
+    phone: '16621109851',                     // 手机号
+    wechat_num: 'yidian0127',                  // 微信号
+    wechat_url: './images/yidian0127.jpg',     // 微信二维码图片
+    wechat_url_alert: './images/yidian0127_a.png',   // 弹框图片
+    icp: '黔ICP备15013097号-2',                       // icp
+    SQURL: 'https://hm.baidu.com/hm.js?0437b6755148339c6bf7e100cb3e3995'// 商桥链接
+  },
+  'http://zyd.tea-life.cn': {
+    company_name: '长沙惠运网络科技有限公司',       // 公司名称
+    phone: '16621109851',                     // 手机号
+    wechat_num: 'yidian3246',                  // 微信号
+    wechat_url: './images/yidian3246.jpg',     // 微信二维码图片
+    wechat_url_alert: './images/yidian3246_a.png',   // 弹框图片
+    //icp: '黔ICP备15013097号-2',                       // icp
+    SQURL: 'https://hm.baidu.com/hm.js?39acf9bdc674b09c75f9d2672b4ae687'  // 商桥链接
+  },
+  'http://zz1.0515175.top': {  // 个人
+    company_name: '',
+    phone: '16621109851',
+    wechat_num: 'yidian4062',
+    wechat_url: './images/yidian4062.jpg',
+    wechat_url_alert: './images/yidian4062_a.png',
+    icp: '苏ICP备17036955号',
+    SQURL: 'https://hm.baidu.com/hm.js?9f2711977878feb9edd84cd2fec7dc2d'
+  },
+  'http://zyd.j74f.cn': {  // 个人
+    company_name: '',
+    phone: '16621109851',
+    wechat_num: 'yidian4062',
+    wechat_url: './images/yidian4062.jpg',
+    wechat_url_alert: './images/yidian4062_a.png',
+    icp: '苏ICP备17036955号',
+    SQURL: 'https://hm.baidu.com/hm.js?9f2711977878feb9edd84cd2fec7dc2d'
+  },
+  'http://yd.xmszsm.cn': {
+    company_name: '厦门首周商贸有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian3246',
+    wechat_url: './images/yidian3246.jpg',
+    wechat_url_alert: './images/yidian3246_a.png',
+    icp: '闽ICP备18029758号-1',
+    // SQURL: 'https://hm.baidu.com/hm.js?52f394e92996c63ed20354f81e6eecbf'
+  },
+  'http://zyd.shengyitong.top': {
+    company_name: '南阳昊霖商贸有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian4059',
+    wechat_url: './images/yidian4059.jpg',
+    wechat_url_alert: './images/yidian4059_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?a4c361d853a85a0fc7b767870cbac5b1'
+  },
+  'http://zyd.fand.top': {
+    company_name: '上海隆兴食品有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian1201',
+    wechat_url: './images/yidian1201.jpg',
+    wechat_url_alert: './images/yidian1201_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?ada1d485013318b4946f956daabb50ee'
+  },
+  'http://zyd1.115133.cn': {
+    company_name: '渭南彩虹教育服务有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian9873',
+    wechat_url: './images/yidian9873.jpg',
+    wechat_url_alert: './images/yidian9873_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?837671da0564c03fc4aa80fc3b7e970b'
+  },
+  'http://zyd.115133.cn': {
+    company_name: '渭南彩虹教育服务有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian3495',
+    wechat_url: './images/yidian3495.jpg',
+    wechat_url_alert: './images/yidian3495_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?24707d93c7c9ca7cad267a38f408fc73'
+  },
+  'http://zyd2.snfsmmaj.cn': {
+    company_name: '文山民创科技有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian6870',
+    wechat_url: './images/yidian6870.jpg',
+    wechat_url_alert: './images/yidian6870_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?188edebb4900cfa548f07d69dcd83b49'
+  },
+  'http://zzy.gzshandi.com': {
+    company_name: '贵州乾美佳亿科技有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian1203',
+    wechat_url: './images/yidian1203.jpg',
+    wechat_url_alert: './images/yidian1203_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?d052ef643879932409628ab2e6fb73e0',
+    statistics: 'https://hm.baidu.com/hm.js?d052ef643879932409628ab2e6fb73e0'
+  },
+  'http://zyd.gzshandi.com': {
+    company_name: '贵州乾美佳亿科技有限公司',
+    phone: '16621109851',
+    wechat_num: 'yidian4179',
+    wechat_url: './images/yidian4179.jpg',
+    wechat_url_alert: './images/yidian4179_a.png',
+    //icp: '闽ICP备18029758号-1',
+    SQURL: 'https://hm.baidu.com/hm.js?726e095d1fff19befb724d236e12cc56'
+  },
+  'http://zyd.98yvip.cn': {
+    company_name: '湛江市元基投资有限公司',
+    phone: '16621109851',                     // 手机号
+    wechat_num: 'yidian0127',                  // 微信号
+    wechat_url: './images/yidian0127.jpg',     // 微信二维码图片
+    wechat_url_alert: './images/yidian0127_a.png',   // 弹框图片
+    // icp: '黔ICP备15013097号-2',                       // icp
+    SQURL: 'https://hm.baidu.com/hm.js?0437b6755148339c6bf7e100cb3e3995'// 商桥链接
+  }
+};
+
+
+
+
+
+
+
+(function () {
+    var originUrl = window.location.origin;
+    var configData = config[originUrl] || config['http://zz.henie.top']
+    console.log(configData, 'configData')
+    if (!configData) {return}
+    var Company = $('.my-company');  //公司名称
+
+    configData.wechat_url_alert && $('#my-custom-qrcode').css('background-image', 'url(' + configData.wechat_url_alert + ')');
+    configData.wechat_url && $('.qrcode-zixun > img').attr('src', configData.wechat_url);
+    configData.phone && $('.qrcode-zixun-phone').html(configData.phone);
+    configData.company_name && ($('.my-company').html(configData.company_name));
+    configData.icp && $('.icp_info').html(configData.icp);
+    configData.wechat_num && $('.wechat_num').html(configData.wechat_num);
+    configData.company_name && Company.html(configData.company_name)
+
+    //百度商桥
+    if (configData.SQURL) {
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = configData.SQURL;
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    }
+
+    //百度统计
+    if (configData.statistics) {
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = configData.statistics;
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    }
+})()
